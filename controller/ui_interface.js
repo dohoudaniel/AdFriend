2a09a3b13ea22a6cb798534d4d5857fe0eae6df5
emmaculate-exceel <emmaculate2exceel@live.com>
2025-02-03 17:35:38 +0100

    file structure

---
+++ controller/ui_interface.js
@@ -0,0 +1,12 @@
+document.getElementById('changeColor').addEventLister('click', () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        chrome.scripting.executeScript({
+            target: { tabId: tabs[0].id },
+            func: changeColor
+        });
+    });
+});
+
+function changeColor() {
+    document.body.style.backgroundColor = "lightblue";
+}

7bdf8d8c288056c0e84d7164b1480d0f316a33e4
emmaculate-exceel <emmaculate2exceel@live.com>
2025-02-11 19:49:26 +0100

    add media content for the blocked add

--- scripts/background_process.js
+++ scripts/background_process.js
@@ -1,3 +1,24 @@
+// List of urls to block ads from
+const list = [
+    '*://*.youtube.com/*",
+    "*://*.canyoublockit.com/*",
+    "*://*.doubleclick.net/*",
+    "*://*.googlesyndication.com/*",
+    "*://*.adnxs.com/*",
+    "*://*.criteo.com/*",
+    "*://*.ads.twitter.com/*",
+    "*://*.facebook.com/ads/*"
+];
+
+// Create a network trigger for the page
+chrome.webRequest.onBeforeRequest.addListener(
+    function(data) {
+        return { cancel: true };
+    },
+    { urls: list },
+    ["blocking"]
+);
+
 chrome.runtime.onInstalled.addListener(() => {
     console.log("Extension installed successfully!");
 });
--- scripts/ui_interface.js
+++ scripts/ui_interface.js
@@ -1,5 +1,76 @@
-document.getElementById('changeColor').addEventListener('click', () => {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+/// Handling content to replace the blocked ads
+function adElement() {
+    // List of possble classname
+    const adClassNames = [
+        '[class*="ad-"]',
+        '[id*="ad-"]',
+        '[class*="advertisement"]',
+        '[class^="ad-"]',
+        '[id^="ad-"]',
+        '[id*="advertisement"]',
+        '.ad-container',
+        '#ad-container'
+    ]
+
+    // loop through each of the classname
+    adClassNames.foreach(function (items) {
+        const elements = document.querySelectorAll(items);
+
+        elements.foreach(function (elements) {
+            // setting up constants for the replacement of the blocked add
+            const height = document.offsetHeight;
+            const width = document.offsetwidth;
+            const div = document.createElement("div");
+            // setting up elements that replaces the ad contents on the webpage 
+            div.className = "adContent";
+            div.style.width = width + "px";
+            div.style.height = height + "px";
+            div.style.boxSizing = "box-sizing";
+            div.style.padding = "10px";
+            div.innerHTML = customCont();
+
+            elements.parentNode = replaceChild(rep_cont, ads);
+        });
+    })
+};
+
+//// function that handles customizations
+function customCont() {
+    const media_content = [
+        {
+            "type": "quote",
+            "content": '"The best way to predict the future is to create it."- Peter Druker'
+        },
+        {
+            "type": "tip",
+            "content": "You can always take a 5-minute break to stretch out and stay healthy!"
+        },
+        {
+            "type": "fact",
+            "content": "Do you know that Dolphins use whistles to identify each other, and respond when they hear their own name?"
+        },
+        {
+            "type": "fact",
+            "content": "Did you know that Antartica is the worlds largest desert ?"
+        },
+        {
+            "type": "fact",
+            "content": "Did you know that slug has 4 noses ?"
+        }
+    ]
+
+    const randomize = contents[Math.floor(Math.random()) * contents.length];
+    return `
+        <div style="background: #f5f5f5 border-radius: 8px; padding: 15px; height: 100%;">
+        <h3 style="margin:0 0 10px 0; color:#333;">${media_content.type.toUpperCase()}
+        <p style="margin: 0; color: #666;">${media_content.content}</p> </div>
+    `;
+}
+
+adElement();
+
+document.getElementById('changeColor').addEventListener('click', function() {
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
         chrome.scripting.executeScript({
             target: { tabId: tabs[0].id },
             func: changeColor
